8f2c4139acd3d6130c91238e322aeb826c50109d
gabrielhamdan <gabriel_hamdan@hotmail.com>
2024-04-28 23:34:27 -0300

    feat: random words

--- main.c
+++ main.c
@@ -8,35 +8,56 @@
 #define MEM_OFFSET 12
 #define COLUMN_LEN 16
 #define MEM_LEN (COLUMN_LEN * 2)
-#define WORD_LEN 8
 #define GARBAGE_CHAR_LIST_LEN 29
+#define MEM_DUMP_LEN MEM_OFFSET * COLUMN_LEN * 2
 
 const char *garbage_char_list = {"!@#$%&*()-_=+[{~^\\|,<.>;:/?}]"};
-const char *rand_wrd = {"word"};
-char mem_dump[MEM_OFFSET];
+const char *rand_wrd = {"WORD"};
+char mem_dump[MEM_DUMP_LEN];
+char mem_dump_chunk[MEM_OFFSET];
 
 int get_random_int(int max_rand) {
-    
     return rand() % max_rand;
+}
+
+void gen_random_garbage_stream() {
+    for(int i = 0; i < MEM_DUMP_LEN; i++)
+        mem_dump[i] = garbage_char_list[get_random_int(strlen(garbage_char_list))];
+}
+
+char * print_garbage_stream(int offset) {
+    for(int i = offset, j = 0; j < MEM_OFFSET; i++, j++)
+        mem_dump_chunk[j] = mem_dump[i];
+    return mem_dump_chunk;
 }
 
 char * print_mem_dump() {
     int wrd_first_char = get_random_int(MEM_OFFSET - strlen(rand_wrd));
-    for(int i = 0; i < MEM_OFFSET; i++)
-        mem_dump[i] = garbage_char_list[get_random_int(strlen(garbage_char_list))];
-
     for(int i = 0; i < strlen(rand_wrd); i++)
         mem_dump[wrd_first_char++] = rand_wrd[i];
 
     return mem_dump;
 }
 
+void populate_garbage_stream() {
+    int r1 = 2, r2 = 16;
+    for(int i = 0; i < 10; i++) {
+        int wrd_first_char = get_random_int(MEM_DUMP_LEN - strlen(rand_wrd) - r1 - r2);
+        for(int j = 0; j < strlen(rand_wrd); j++)
+            mem_dump[wrd_first_char++] = rand_wrd[j];
+        r1 += get_random_int(r1);
+        r2 += get_random_int(r2);
+    }
+}
+
 void print_display() {
     int first_mem_add = get_random_int(MAX_RAND - MIN_RAND);
+    gen_random_garbage_stream();
+    populate_garbage_stream();
 
     for(int i = 0; i < COLUMN_LEN; i++) {
-        printf("0x%04X  %s\t", first_mem_add, print_mem_dump());
-        printf("0x%04X  %s\n", (first_mem_add + COLUMN_LEN * MEM_OFFSET), print_mem_dump());
+        printf("0x%04X  %s\t", first_mem_add, print_garbage_stream(i * MEM_OFFSET));
+        printf("0x%04X  %s\n", (first_mem_add + COLUMN_LEN * MEM_OFFSET), print_garbage_stream(i * MEM_OFFSET + COLUMN_LEN * MEM_OFFSET));
         first_mem_add += MEM_OFFSET;
     }
 }
